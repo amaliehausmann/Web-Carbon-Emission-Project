23e1e6974ced8a9ea6ece6ce715e3e3ee05aa473
amaliehausmann <116646736+amaliehausmann@users.noreply.github.com>
2024-06-23 16:52:08 +0200

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,37 @@ async function greenCheck(hostName) {
     return res;
 }
 
-let greenCheckData = await greenCheck('www.techcollege.dk');
+function buildGreenCheck (inputId, buttonId, appendElementId){
+    const website = document.getElementById(inputId);
+    const button = document.getElementById(buttonId);
+    const greenCheckBox = document.createElement('section');
+    greenCheckBox.classList = 'greenCheckBox';
+    const appendElement = document.getElementById(appendElementId);      
+    const greenText = document.createElement('p');
+    const inputText = document.createElement('h2');
+    const anotherButton = document.createElement('button');
 
-console.log(greenCheckData);
+    greenCheckBox.appendChild(inputText);
+    greenCheckBox.appendChild(greenText);
+    greenCheckBox.appendChild(anotherButton);
+    appendElement.appendChild(greenCheckBox);
 
+    button.addEventListener('click', async () => {
+        greenCheckBox.style.display = 'block';
+        const inputtedWebsite = website.value;
+        let greenCheckData = await greenCheck(inputtedWebsite);
+        inputText.innerText = 'Website: ' + inputtedWebsite;
+        greenText.innerText = greenCheckData.green ? 'This website is green!' : 'This website is not green or we dont have the neccessary data';
+        anotherButton.innerText = 'Try another Website!';
 
+        anotherButton.addEventListener('click', () => {
+            greenCheckBox.style.display = 'none';
+        })
+
+    });
+}
+
+buildGreenCheck('greenCheck', 'greenCheckButton', 'greencheckBox');
 
 
 // CO2 Check
@@ -21,6 +47,43 @@ async function co2Check(address) {
     return res;
 }
 
-let co2CheckData = await co2Check('www.techcollege.dk');
+function buildCo2Check (inputId, buttonId, appendElementId) {
+    const website = document.getElementById(inputId);
+    const button = document.getElementById(buttonId);
+    const co2CheckBox = document.createElement('section');
+    co2CheckBox.classList = 'co2CheckBox';
+    const appendElement = document.getElementById(appendElementId);
+    const country = document.createElement('p');
+    const intensity = document.createElement('p');
+    const level = document.createElement('p');
+    const anotherbutton = document.createElement('button');
+    const websitetext = document.createElement('h2');
 
-console.log(co2CheckData);
+    co2CheckBox.appendChild(websitetext);
+    co2CheckBox.appendChild(country);
+    co2CheckBox.appendChild(intensity);
+    co2CheckBox.appendChild(level);
+    co2CheckBox.appendChild(anotherbutton);
+    
+    appendElement.appendChild(co2CheckBox);
+
+
+    button.addEventListener('click', async () => {
+        co2CheckBox.style.display = 'block';
+        const inputtedWebsite = website.value;
+        let co2CheckData = await co2Check(inputtedWebsite);
+        websitetext.innerText = 'Website: ' + inputtedWebsite;
+        country.innerText = 'Country: ' + co2CheckData.country_name;
+        intensity.innerText = 'Co2 Intensity: ' + co2CheckData.carbon_intensity;
+        level.innerText = 'Co2 Intensity Type: ' + co2CheckData.carbon_intensity_type;
+        anotherbutton.innerText = 'Try another website!';
+
+        anotherbutton.addEventListener('click', () => {
+            co2CheckBox.style.display = 'none';
+        }); 
+
+    });
+    
+}
+
+buildCo2Check ('co2Calculator', 'calculateButton', 'calculate');
